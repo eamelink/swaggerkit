61ede9ee6b32caa0c2263356c34ec78dc2d4ba0d
Erik Bakker <erik.bakker@lunatech.com>
2012-07-26 10:18:14 +0200

    Add publish configuration

--- project/Build.scala
+++ project/Build.scala
@@ -2,11 +2,13 @@ import sbt._
 import Keys._
 
 object Settings {
-  val name = "swaggerkit"
-  
+  val name = "swaggerkit" 
+
   val buildSettings = Project.defaultSettings ++ Seq(
     version := "0.1",
-    scalaVersion := "2.9.1")
+    scalaVersion := "2.9.1",
+    organization := "net.eamelink"
+  )
 }
 
 object Resolvers {
@@ -28,10 +30,19 @@ object ApplicationBuild extends Build {
   ) aggregate (core, play2)
 
   lazy val core = Project(name + "-core", file("core"), settings = buildSettings ++ Seq(
+    publishTo <<= version { (v: String) => 
+      val path = if(v.trim.endsWith("SNAPSHOT")) "snapshots-public" else "releases-public"
+      Some(Resolver.url("Lunatech Artifactory", new URL("http://artifactory.lunatech.com/artifactory/%s/" format path)))
+    },
     resolvers := Seq(typesafeRepo),
     libraryDependencies ++= Seq(specs)))
 
   lazy val play2 = Project(name + "-play2", file("play2"), settings = buildSettings ++ Seq(
+    publishTo <<= version { (v: String) => 
+      val path = if(v.trim.endsWith("SNAPSHOT")) "snapshots-public" else "releases-public"
+      Some(Resolver.url("Lunatech Artifactory", new URL("http://artifactory.lunatech.com/artifactory/%s/" format path)))
+    },
     resolvers := Seq(typesafeRepo),
     libraryDependencies ++= Seq(play, specs))) dependsOn (core)
+
 }
