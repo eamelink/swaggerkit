685690048e23c58b3060190bd190c7e8c506579d
Erik Bakker <erik.bakker@lunatech.com>
2013-02-14 16:07:21 +0100

    Bump dependencies to Scala 2.10 and Play 2.1

--- project/Build.scala
+++ project/Build.scala
@@ -5,8 +5,8 @@ object Settings {
   val name = "swaggerkit" 
 
   val buildSettings = Project.defaultSettings ++ Seq(
-    version := "0.1.1",
-    scalaVersion := "2.9.1",
+    version := "0.2.0-SNAPSHOT",
+    scalaVersion := "2.10.0",
     organization := "net.eamelink"
   )
 }
@@ -16,8 +16,8 @@ object Resolvers {
 }
 
 object Dependencies {
-  lazy val play = "play" %% "play" % "2.0"
-  lazy val specs = "org.specs2" %% "specs2" % "1.9" % "test"
+  lazy val play = "play" %% "play" % "2.1.0"
+  lazy val specs = "org.specs2" %% "specs2" % "1.13" % "test"
 }
 
 object ApplicationBuild extends Build {
